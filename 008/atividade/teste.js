ceee41be522d8a2da66db7ba24bf567bbbe1faeb
Gustavo <gu01stavo@gmail.com>
2025-03-24 22:09:55 -0300

    008

--- 008/atividade/teste.js
+++ 008/atividade/teste.js
@@ -1,3 +1,8 @@
+import { deepStrictEqual, strictEqual } from "assert";
+import{ bubbleSort, buscaBinaria } from "./solucaoBubble.js";
+
+// Criar dados parar testes 
+// Dados de teste
 let pessoas = [
     { nome: "Ana", nascimento: "1990-05-12", tipoSanguineo: "O+", cpf: "12345678900", telefone: "999999999" },
     { nome: "Carlos", nascimento: "1985-07-23", tipoSanguineo: "A-", cpf: "23456789011", telefone: "988888888" },
@@ -7,4 +12,17 @@ let pessoas = [
     { nome: "Pedro", nascimento: "1993-12-05", tipoSanguineo: "A+", cpf: "67890123455", telefone: "944444444" }
 ];
 
-// BUSCA BINÁRIA POR JOÃO, PARA ISSO A LISTA PRECISA ESTAR ORDENADA POR CPF
+// Verificar se os testes são atendidos
+
+let pessoasOrdenadas = bubbleSort(pessoas);
+let resultadoEsperando = ["Ana", "Carlos", "Fernanda", "João","Maria","Pedro"];
+
+deepStrictEqual(pessoasOrdenadas.map(p => p.nome), resultadoEsperando, " ❌ Erro na ordenação!");
+
+// Teste 2: Busca Binária (Nome Existente)
+let objetoResultado = buscaBinaria(pessoasOrdenadas, "Ana");
+strictEqual(objetosResultado.nome, "Ana", "❌ Erro na busca binária (nome não existente)");
+
+
+
+console.log(" ✅ Todos os teste passaram!")
